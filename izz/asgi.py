96e38e12b740b3097890f444ef4dbb6ffa1735be
ihorkryvoruchko <igor.kryvoruchko.88@gmail.com>
2026-06-23 14:23:10 +0200

    WebSockets Grundlagen

--- izz/asgi.py
+++ izz/asgi.py
@@ -8,9 +8,23 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import izz.routing  # Das erstellen wir im nächsten Schritt
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'izz.settings')
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    # Normales HTTP (z.B. für die Admin-Oberfläche)
+    "http": django_asgi_app,
+
+    # WebSockets (für unsere Echtzeit-Züge)
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            izz.routing.websocket_urlpatterns
+        )
+    ),
+})
